2fa4a120db1360a41c800e13803b716f90aa2025
Luciana Sugeta <lulusugeta@gmail.com>
2021-03-23 12:49:40 -0400

    commit tree 0571f388b912ac579127d32aa27211fe0fc5f58a

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,11 @@
 require_relative "./vehicle.rb"
+class Car < Vehicle
+    #We use the < to inherit the Car class from Vehicle.
+    #instances of Car class inherit all of the Vehicle methods and therefore have access to them.
+
+    #returns 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+    #overwrite the inherited go method with one specific to Car
+    def go
+        "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+    end
+end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,21 @@
 class Vehicle
-
+    #has a wheel_size
+    #has a wheel_number
+    attr_accessor :wheel_size, :wheel_number
+    
+    #initializes with two arguments, wheel size and number
+    def initialize(wheel_size, wheel_number)
+        @wheel_size = wheel_size
+        @wheel_number = wheel_number
+    end
+    
+    #returns 'vrrrrrrrooom!
+    def go
+        "vrrrrrrrooom!"
+    end
+    
+    #returns 'filling up!'
+    def fill_up_tank
+        "filling up!"
+    end
 end
